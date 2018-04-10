fdd287f09881632d1ced1daa296fdbbdd1574aa9
Denis Sadowskiy <92denis-s@mail.ru>
2018-04-11 00:01:00 +0300

    add material to my project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
+import {MatToolbarModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -10,8 +12,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
