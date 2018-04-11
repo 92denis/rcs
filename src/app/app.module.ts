111070ae3a45890a9884cd87b5bc6ae020eccfe5
Denis Sadowskiy <92denis-s@mail.ru>
2018-04-11 14:44:46 +0300

    create component repository-item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { AppComponent } from './app.component';
 import { RepositoryListComponent } from './repository-list/repository-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RcsService } from './rcs.service';
+import { RepositoryItemComponent } from './repository-item/repository-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RepositoryListComponent
+    RepositoryListComponent,
+    RepositoryItemComponent
   ],
   imports: [
     BrowserModule,
