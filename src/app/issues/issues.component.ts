2f0f8c5f1bddaa3f18ce06fd46aa1660099d787e
Denis Sadowskiy <92denis-s@mail.ru>
2018-04-12 20:59:26 +0300

    create issues array

--- src/app/issues/issues.component.ts
+++ src/app/issues/issues.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs/Subscription';
+import { Issue } from '../issue';
+import { RcsService } from '../rcs.service';
 
 @Component({
   selector: 'app-issues',
@@ -11,13 +13,14 @@ export class IssuesComponent implements OnInit {
 
   name: string;
   subscription: Subscription;
+  issues: Issue[];
 
-  constructor(private activateRoute: ActivatedRoute) {
-
+  constructor(private activateRoute: ActivatedRoute, private rcsServise: RcsService) {
     this.subscription = this.activateRoute.params.subscribe(params => this.name = params['name']);
   }
 
   ngOnInit() {
+    this.rcsServise.getIssues(this.name).subscribe(issues => this.issues = issues);
   }
 
 }
