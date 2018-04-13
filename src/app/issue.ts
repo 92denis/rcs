e0e78d1337bafd19d3bd6ec00b5424b29e0128d7
Denis Sadowskiy <92denis-s@mail.ru>
2018-04-13 16:06:10 +0300

    initialized properties in constructor of Issue class

--- src/app/issue.ts
+++ src/app/issue.ts
@@ -4,4 +4,9 @@ export class Issue {
     public comments: string[];
     public user: object;
     public body: string;
+    constructor() {
+        this.id = Math.random() * (100000 - 500) + 500;
+        this.comments = [];
+        this.user = { login: 'Unknown', avatar_url: 'https://pp.userapi.com/c626421/v626421094/2f8b/XrJWtAPVxe4.jpg' };
+    }
 }
