74b94312c1474d415081323d54200745f894e451
Denis Sadowskiy <92denis-s@mail.ru>
2018-04-13 19:19:54 +0300

    RepositoryList: create method sortA_Z

--- src/app/repository-list/repository-list.component.ts
+++ src/app/repository-list/repository-list.component.ts
@@ -11,17 +11,36 @@ export class RepositoryListComponent implements OnInit {
 
   repositories: Repository[] = [];
   languagesSearch: string;
+  sort = true;
+  buttonName: string;
 
   constructor(private rcsService: RcsService) {
     this.languagesSearch = '';
+    this.buttonName = 'Sort descending';
   }
 
   deleteRepo(id) {
     this.repositories = this.repositories.filter(repo => repo.id !== id);
   }
 
+  sortA_Z() {
+    this.rcsService.getRepositories().subscribe(data => {
+      this.repositories = data;
+      this.repositories.sort((a, b) => {
+        if (this.sort) {
+          this.buttonName = 'Sort descending';
+          return +(a.name.toLowerCase() > b.name.toLowerCase()) || +(a.name.toLowerCase() === b.name.toLowerCase()) - 1;
+        } else {
+          this.buttonName = 'Sort ascending';
+          return +(a.name.toLowerCase() < b.name.toLowerCase()) || +(a.name.toLowerCase() === b.name.toLowerCase()) - 1;
+        }
+      });
+    });
+    this.sort = !this.sort;
+  }
+
   ngOnInit(): void {
-    this.rcsService.getRepositories().subscribe(data => this.repositories = data );
+    this.rcsService.getRepositories().subscribe(data => this.repositories = data);
   }
 
 }
